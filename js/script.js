eb6afac7e6ecdd12b77afb9eaee4cc0f71dde3ad
Eugene Cheung <echeung@infusion.com>
2015-09-21 20:05:44 -0400

    requestAnimationFrame

--- js/script.js
+++ js/script.js
@@ -1,56 +1,57 @@
-particlesJS('particles', {
-  'particles': {
-    'number': {
-      'value': 50,
-      'density': {
+requestAnimationFrame(function () {
+  particlesJS('particles', {
+    'particles': {
+      'number': {
+        'value': 50,
+        'density': {
+          'enable': true,
+          'value_area': 900
+        }
+      },
+      'color': {
+        'value': '#9aa7ba'
+      },
+      'opacity': {
+        'value': 0.5,
+        'anim': {
+          'enable': false,
+          'speed': 1,
+          'opacity_min': 0.1,
+          'sync': false
+        }
+      },
+      'size': {
+        'value': 3,
+        'random': true,
+        'anim': {
+          'enable': false,
+          'speed': 40,
+          'size_min': 0.1,
+          'sync': false
+        }
+      },
+      'line_linked': {
         'enable': true,
-        'value_area': 800
+        'distance': 150,
+        'color': '#9aa7ba',
+        'opacity': 0.4,
+        'width': 1
+      },
+      'move': {
+        'enable': true,
+        'speed': 5,
+        'direction': 'none',
+        'random': false,
+        'straight': false,
+        'out_mode': 'out',
+        'bounce': false,
+        'attract': {
+          'enable': false,
+          'rotateX': 600,
+          'rotateY': 1200
+        }
       }
     },
-    'color': {
-      'value': '#9aa7ba'
-    },
-    'opacity': {
-      'value': 0.5,
-      'random': false,
-      'anim': {
-        'enable': false,
-        'speed': 1,
-        'opacity_min': 0.1,
-        'sync': false
-      }
-    },
-    'size': {
-      'value': 3,
-      'random': true,
-      'anim': {
-        'enable': false,
-        'speed': 40,
-        'size_min': 0.1,
-        'sync': false
-      }
-    },
-    'line_linked': {
-      'enable': true,
-      'distance': 150,
-      'color': '#9aa7ba',
-      'opacity': 0.4,
-      'width': 1
-    },
-    'move': {
-      'enable': true,
-      'speed': 5,
-      'direction': 'none',
-      'random': false,
-      'straight': false,
-      'out_mode': 'out',
-      'bounce': false,
-      'attract': {
-        'enable': false,
-        'rotateX': 600,
-        'rotateY': 1200
-      }
-    }
-  },
-  'retina_detect': true
+    'retina_detect': true
+  });
 });
